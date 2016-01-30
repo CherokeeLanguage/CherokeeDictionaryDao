d4e25d867d484bca3f1f0bf761cbd0c7a840dbca
mjoyner <mjoyner@vbservices.net>
2016-01-30 10:25:33 -0500

    add simple utility to translit syllabary for indexing purposes

--- CherokeeDictionaryDao/src/main/java/net/cherokeedictionary/util/DaoUtils.java
+++ CherokeeDictionaryDao/src/main/java/net/cherokeedictionary/util/DaoUtils.java
@@ -1,6 +1,8 @@
 package net.cherokeedictionary.util;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DaoUtils {
 	
@@ -8,6 +10,7 @@ public class DaoUtils {
 	
 	static {
 		json = new JsonConverter();
+		_chr2translit_init();
 	}
 	
 	public static String properFormedPronunciation(String pronunciation) {
@@ -46,5 +49,147 @@ public class DaoUtils {
 	public static java.util.Date asDate(java.sql.Timestamp sqlTimestamp) {
 		return sqlTimestamp != null ? new java.util.Date(sqlTimestamp.getTime())
 				: null;
+	}
+	
+	public static String syllabaryTranslit(String syllabary) {
+		if (syllabary==null) {
+			return null;
+		}
+		StringBuilder sb = new StringBuilder(syllabary.length()*3);
+		for (char character: syllabary.toCharArray()) {
+			String letter = String.valueOf(character);
+			if (chr2trans.containsKey(letter)){
+				sb.append(chr2trans.get(letter));
+				continue;
+			}
+			sb.append(letter);
+		}
+		return sb.toString();
+	}
+	
+	private static final Map<String, String> chr2trans = new HashMap<>();
+	private static void _chr2translit_init() {
+		int ix = 0;
+		String letter;
+		String prefix;
+		char chrStart = 'Ꭰ';
+		String[] vowels = new String[6];
+
+		vowels[0] = "a";
+		vowels[1] = "e";
+		vowels[2] = "i";
+		vowels[3] = "o";
+		vowels[4] = "u";
+		vowels[5] = "v";
+
+		for (ix = 0; ix < 6; ix++) {
+			letter = Character.toString((char) (chrStart + ix));
+			chr2trans.put(letter, vowels[ix]);
+		}
+
+		chr2trans.put("Ꭶ", "ga");
+		chr2trans.put("Ꭷ", "ka");
+
+		prefix = "g";
+		chrStart = 'Ꭸ';
+		for (ix = 1; ix < 6; ix++) {
+			letter = Character.toString((char) (chrStart + ix - 1));
+			chr2trans.put(letter, prefix + vowels[ix]);
+		}
+
+		prefix = "h";
+		chrStart = 'Ꭽ';
+		for (ix = 0; ix < 6; ix++) {
+			letter = Character.toString((char) (chrStart + ix));
+			chr2trans.put(letter, prefix + vowels[ix]);
+		}
+
+		prefix = "l";
+		chrStart = 'Ꮃ';
+		for (ix = 0; ix < 6; ix++) {
+			letter = Character.toString((char) (chrStart + ix));
+			chr2trans.put(letter, prefix + vowels[ix]);
+		}
+
+		prefix = "m";
+		chrStart = 'Ꮉ';
+		for (ix = 0; ix < 5; ix++) {
+			letter = Character.toString((char) (chrStart + ix));
+			chr2trans.put(letter, prefix + vowels[ix]);
+		}
+
+		chr2trans.put("Ꮎ", "na");
+		chr2trans.put("Ꮏ", "hna");
+		chr2trans.put("Ꮐ", "nah");
+
+		prefix = "n";
+		chrStart = 'Ꮑ';
+		for (ix = 1; ix < 6; ix++) {
+			letter = Character.toString((char) (chrStart + ix - 1));
+			chr2trans.put(letter, prefix + vowels[ix]);
+		}
+
+		prefix = "qu";
+		chrStart = 'Ꮖ';
+		for (ix = 0; ix < 6; ix++) {
+			letter = Character.toString((char) (chrStart + ix));
+			chr2trans.put(letter, prefix + vowels[ix]);
+		}
+
+		chr2trans.put("Ꮜ", "sa");
+		chr2trans.put("Ꮝ", "s");
+
+		prefix = "s";
+		chrStart = 'Ꮞ';
+		for (ix = 1; ix < 6; ix++) {
+			letter = Character.toString((char) (chrStart + ix - 1));
+			chr2trans.put(letter, prefix + vowels[ix]);
+		}
+
+		chr2trans.put("Ꮣ", "da");
+		chr2trans.put("Ꮤ", "ta");
+		chr2trans.put("Ꮥ", "de");
+		chr2trans.put("Ꮦ", "te");
+		chr2trans.put("Ꮧ", "di");
+		chr2trans.put("Ꮨ", "ti");
+		chr2trans.put("Ꮩ", "do");
+		chr2trans.put("Ꮪ", "du");
+		chr2trans.put("Ꮫ", "dv");
+		chr2trans.put("Ꮬ", "dla");
+
+		prefix = "hl";
+		chrStart = 'Ꮭ';
+		for (ix = 0; ix < 6; ix++) {
+			letter = Character.toString((char) (chrStart + ix));
+			chr2trans.put(letter, prefix + vowels[ix]);
+		}
+
+		prefix = "tl";
+		chrStart = 'Ꮭ';
+		for (ix = 0; ix < 6; ix++) {
+			letter = Character.toString((char) (chrStart + ix));
+			chr2trans.put(letter, prefix + vowels[ix]);
+		}
+		
+		prefix = "ts";
+		chrStart = 'Ꮳ';
+		for (ix = 0; ix < 6; ix++) {
+			letter = Character.toString((char) (chrStart + ix));
+			chr2trans.put(letter, prefix + vowels[ix]);
+		}
+
+		prefix = "w";
+		chrStart = 'Ꮹ';
+		for (ix = 0; ix < 6; ix++) {
+			letter = Character.toString((char) (chrStart + ix));
+			chr2trans.put(letter, prefix + vowels[ix]);
+		}
+
+		prefix = "y";
+		chrStart = 'Ꮿ';
+		for (ix = 0; ix < 6; ix++) {
+			letter = Character.toString((char) (chrStart + ix));
+			chr2trans.put(letter, prefix + vowels[ix]);
+		}
 	}
 }
