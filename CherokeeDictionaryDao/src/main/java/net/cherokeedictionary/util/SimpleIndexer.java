3df9451ea04063060e5c80ee5eaeb38cb08256c8
mjoyner <mjoyner@vbservices.net>
2016-01-30 11:18:01 -0500

    simple cli indexer

---
+++ CherokeeDictionaryDao/src/main/java/net/cherokeedictionary/util/SimpleIndexer.java
@@ -0,0 +1,43 @@
+package net.cherokeedictionary.util;
+
+import java.util.List;
+
+import net.cherokeedictionary.dao.DaoCherokeeDictionary;
+import net.cherokeedictionary.model.DictionaryEntry;
+
+public class SimpleIndexer {
+	
+	private static final DaoCherokeeDictionary dao = DaoCherokeeDictionary.dao;
+	static {
+		DaoCherokeeDictionary.Util.init();
+	}
+	
+	public static void main(String[] args) {
+		List<DictionaryEntry> forIndexing;
+		int size=0;
+		int sum=0;
+		do {
+			forIndexing=dao.needsIndexing(100);
+			if (forIndexing==null) {
+				break;
+			}
+			size = forIndexing.size();
+			sum+=size;
+			System.out.println("Indexing "+size+" records.");
+			
+			System.out.println("\tAdded "+DaoUtils.getInsertCount(dao.addNewIndexLatinEntriesById(forIndexing))+" Latin/Translit records.");
+			System.out.println("\tUpdated "+DaoUtils.getInsertCount(dao.updateIndexLatinEntriesById(forIndexing))+" Latin/Translit records.");
+			
+			System.out.println("\tAdded "+DaoUtils.getInsertCount(dao.addNewIndexSyllabaryEntriesById(forIndexing))+" Syllabary records.");
+			System.out.println("\tUpdated "+DaoUtils.getInsertCount(dao.updateIndexSyllabaryEntriesById(forIndexing))+" Syllabary records.");
+			
+			System.out.println("\tAdded "+DaoUtils.getInsertCount(dao.addNewIndexEnglishEntriesById(forIndexing))+" English records.");
+			System.out.println("\tUpdated "+DaoUtils.getInsertCount(dao.updateIndexEnglishEntriesById(forIndexing))+" English records.");
+			
+			System.out.println("\t\tIndex marked "+dao.updateIndexMarksById(forIndexing)+" records.");
+			System.out.println();
+		} while (size!=0);
+		System.out.println("No more records need indexing.");
+		System.out.println("\t"+sum+" records were processed.");
+	}
+}
